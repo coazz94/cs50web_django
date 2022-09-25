d4dd329374d46f428b57cdc14209b73995410e39
coazz <aco94@hotmail.com>
2022-09-25 11:53:54 +0200

    Updates

--- hello/urls.py
+++ hello/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index") 
+    path("", views.index, name="index"),
+    path("aleks", views.aleks, name="aleks"),
+    path("<str:name>", views.greet, name="greet"),
 ]
 
 
--- hello/views.py
+++ hello/views.py
@@ -1,8 +1,17 @@
-from http.client import HTTPResponse
+from curses.ascii import HT
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+#Create your views here.
 #request the user made
 def index(request): 
-    return HTTPResponse("Hello, world!")
+    return render(request, "hello/index.html")
+
+def aleks(request):
+    return HttpResponse("Hello, Aleks")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
+
